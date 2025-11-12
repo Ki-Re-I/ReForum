f9cb6e8b75180ba953c1f9f004b7940a45654f26
HuangWeiLong <1492930634@qq.com>
2025-11-12 17:09:57 +0800

    feat: add changelog navigation link in sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import { FaHome } from 'react-icons/fa'
+import { FaHome, FaInfoCircle, FaEnvelope, FaHistory } from 'react-icons/fa'
 import './Sidebar.css'
 
 const Sidebar = () => {
@@ -15,6 +15,27 @@ const Sidebar = () => {
         >
           <FaHome className="nav-icon" />
           <span>首页</span>
+        </Link>
+        <Link
+          to="/about"
+          className={`nav-item ${location.pathname === '/about' ? 'active' : ''}`}
+        >
+          <FaInfoCircle className="nav-icon" />
+          <span>关于我们</span>
+        </Link>
+        <Link
+          to="/contact"
+          className={`nav-item ${location.pathname === '/contact' ? 'active' : ''}`}
+        >
+          <FaEnvelope className="nav-icon" />
+          <span>联系我们</span>
+        </Link>
+        <Link
+          to="/changelog"
+          className={`nav-item ${location.pathname === '/changelog' ? 'active' : ''}`}
+        >
+          <FaHistory className="nav-icon" />
+          <span>更新日志</span>
         </Link>
       </nav>
     </aside>
